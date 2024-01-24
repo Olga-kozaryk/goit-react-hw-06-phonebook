cef8f9980ad3036c19807a50410987aa02e0c617
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-01-24 17:52:52 +0200

    up ContactForm, create redux

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { BtnForm } from "./BtnForm.styled";
 import { FormStyled } from "./ContactForm.styled";
 import { LabelStyled } from "./LabelForm.styled";
+import { nanoid } from "@reduxjs/toolkit";
+import { addContact } from "../../redux/contactsSlice";
 
-export const ContactForm = ({addContact}) => {
-const [name, setName] = useState('');
-const [number, setNumber] = useState('');
 
-const handleChange = e => {
-  const{name, value } = e.currentTarget;
-  name === 'name' ? setName(value) : setNumber(value);
-};
+export const ContactForm = () => {
+const dispatch = useDispatch();  
 
 const handleSubmit = e => {
   e.preventDefault();
-  addContact({ name, number });
-  setName('');
-  setNumber('');
+
+  const form = e.target;
+  const contact ={
+    name: form.el.name.value,
+    number: form.el.number.value,
+    id: nanoid(),
+  }
+
+    dispatch(addContact(contact));
+
+    form.reset();
 }
     
 return (
@@ -27,8 +32,8 @@ return (
       <input
       type="text"
       name="name"
-      value={name}
-      onChange={handleChange}
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
       required
       />
     </LabelStyled>
@@ -38,8 +43,8 @@ return (
       <input
         type="text"
         name="number"
-        value={number}
-        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
     </LabelStyled>
